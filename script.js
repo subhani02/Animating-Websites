f3d997921d9a32850f8330537ddd80be7d082aa4
Md Subhani <mdsubhani02@gmail.com>
2023-08-09 22:41:48 +0530

    Code Added

---
+++ script.js
@@ -0,0 +1,106 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove", function (dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x- 150 + "px"
+    blur.style.top = dets.y - 150 + "px"
+})
+
+// var h4all = document.querySelectorALL("#nav h4")
+// h4all.forEach(function(elem){
+//     elem.addEventListener("mouseenter",function(){
+//         crsr.style.scale = 3,
+//         crsr.style.border = "1px solid #fff",
+//         crsr.style.backgroundColor = "transparent"
+
+//     })
+// })
+
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    duration:0.5,
+    height:"100px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1,
+    }
+})
+gsap.to("#main",{
+    backgroundColor : "#000",
+    scrollTrigger :{
+        trigger : "#main",
+        scroller : "body",
+        markers : true,
+        start : "top -25%",
+        end : "top -70%",
+        scrub : 1,
+    }
+})
+gsap.from("#about-us img, #about-us-in",{
+    y:90,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        markers: true,
+        start:"top 60%",
+        end:"top 57%",
+        scrub:1
+    }
+
+})
+gsap.from(".card",{
+    sacle:0.8,
+    opacity:0,
+    duration:1,
+    stagger:0.1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        markers: true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1
+    }
+})
+gsap.from("#colom1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colom1",
+        scroller:"body",
+        markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+gsap.from("#colom2",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colom1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        // markers:true,
+        start:"top 90%",
+        end:"top 85%",
+        scrub:3
+    }
+})
